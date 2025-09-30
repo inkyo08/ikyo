4be58076d53004633e8803c517e5b3483f33f737
Inkyo Jung <inkyo08@proton.me>
2025-09-30 19:38:05 +0900

    arch refactor

--- Source/main.swift
+++ Source/main.swift
@@ -1,5 +1,14 @@
-let game = GameLoop()
+@main
+class GameLoop {
+  static func main() {
+    let game = GameLoop()
 
-game.initialize()
-game.update()
-game.exit()
+    game.initialize()
+    game.update()
+    game.exit()
+  }
+
+  func initialize() {}
+  func update() {}
+  func exit() {}
+}
