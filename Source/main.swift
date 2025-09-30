fa6b5534f014a81414f56f9abc5782679ac4d4ad
Inkyo Jung <inkyo08@proton.me>
2025-09-30 19:45:16 +0900

    arch refactor

--- Source/main.swift
+++ Source/main.swift
@@ -1,5 +1,6 @@
+// 이 엔진은 엔진 인스턴스와 게임 인스턴스를 동일하게 취급하며, Main이 곧 게임 루프의 역할을 수행합니다.
 @main
-class GameLoop {
+final class GameLoop {
   static func main() {
     let game = GameLoop()
 
@@ -8,7 +9,16 @@ class GameLoop {
     game.exit()
   }
 
+  var doFrame = false
+
+  // MARK: - Initialize
   func initialize() {}
-  func update() {}
+
+  // MARK: - Update
+  func update() {
+    while doFrame {}
+  }
+
+  // MARK: - Exit
   func exit() {}
 }
