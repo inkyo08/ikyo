c028e634bade744afd5d59967640fe96fb645c7c
inkyo08 <inkyo08@proton.me>
2025-10-03 23:16:42 +0900

    removed glfw

--- Package.swift
+++ Package.swift
@@ -7,30 +7,15 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-atomics", from: "1.3.0")
     ],
     targets: [
-        .systemLibrary(
-            name: "CGLFW",
-            path: "include",
-            pkgConfig: "glfw3"
-        ),
         .executableTarget(
             name: "ikyo",
             dependencies: [
-                "CGLFW",
                 .product(name: "Atomics", package: "swift-atomics")
             ],
             path: "Source",
             swiftSettings: [
                 .unsafeFlags(["-parse-as-library"]),
             ],
-            linkerSettings: [
-                .unsafeFlags(["-L\(Context.packageDirectory)/Libraries/GLFW/mac/arm64"], .when(platforms: [.macOS])),
-                .linkedLibrary("glfw3", .when(platforms: [.macOS])),
-                .linkedFramework("Cocoa", .when(platforms: [.macOS])),
-                .linkedFramework("IOKit", .when(platforms: [.macOS])),
-                .linkedFramework("CoreVideo", .when(platforms: [.macOS])),
-                .linkedFramework("CoreFoundation", .when(platforms: [.macOS])),
-                .unsafeFlags(["-L\(Context.packageDirectory)/Libraries/GLFW/windows/x64"], .when(platforms: [.windows]))
-            ]
         ),
     ]
 )
