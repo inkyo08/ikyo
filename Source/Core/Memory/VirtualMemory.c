7de6a14f2534d04cf773a2082898d0c3214d8d9d
Inkyo Jung <ikyoarch@gmail.com>
2025-11-11 02:48:20 +0900

    Windows VirtualMemory 구현 완료

--- Source/Core/Memory/VirtualMemory.c
+++ Source/Core/Memory/VirtualMemory.c
@@ -1,8 +1,58 @@
-#include "VirtualMemory.h"
+﻿#include "VirtualMemory.h"
 
 #include <assert.h>
 
 #if _WIN32
+
+#include <Windows.h>
+#include <sysinfoapi.h>
+
+const size_t systemPageSize = 4 * 1024;
+
+void VM_Initialize()
+{
+  SYSTEM_INFO si;
+  GetSystemInfo(&si);
+  assert(systemPageSize == si.dwPageSize);
+}
+
+void * VM_Reserve(size_t reserveSize, const size_t alignment) {
+  assert((systemPageSize & (systemPageSize - 1)) == 0);
+  reserveSize = (reserveSize + (systemPageSize - 1)) & ~(systemPageSize - 1);
+  uintptr_t addr;
+
+  do
+  {
+    addr = (uintptr_t)VirtualAlloc(NULL, reserveSize, MEM_RESERVE, PAGE_READWRITE);
+    if (!addr)
+      break;
+
+    const uintptr_t alignedAddr = alignment == 0 ? addr :
+    ((addr + (alignment - 1)) & ~(alignment - 1));
+
+    if ((alignedAddr - addr) > 0)
+    {
+      VirtualFree((void*)addr, 0, MEM_RELEASE);
+      addr = (uintptr_t)VirtualAlloc((void*)alignedAddr, reserveSize, MEM_RESERVE, PAGE_READWRITE);
+    }
+  } while (!addr);
+
+  return (void*)addr;
+}
+
+void VM_Unreserve(void *base, size_t reservedSize, size_t alignment) {
+  VirtualFree(base, 0, MEM_RELEASE);
+}
+
+void VM_MapPages(void *base, const size_t size) {
+  VirtualAlloc(base, size, MEM_COMMIT, PAGE_READWRITE);
+}
+
+void VM_UnmapPages(void *base, const size_t size) {
+  VirtualFree(base, size, MEM_DECOMMIT);
+}
+
+
 #elif __APPLE__ || __linux__
 
 #include <sys/mman.h>
@@ -18,7 +68,7 @@ do { \
 } while(0)
 #endif
 
-const static size_t systemPageSize = 4 * 1024;
+const size_t systemPageSize = 4 * 1024;
 
 void VM_Initialize() { assert(systemPageSize == sysconf(_SC_PAGESIZE)); }
 
@@ -34,7 +84,7 @@ void* VM_Reserve(size_t reserveSize, const size_t alignment)
     return ptr;
   }
 
-  // mmap은 요청된 정렬 주소에 무조건 할당되는 게 아니라서 수동으로 조정해야 함.
+  // mmap은 요청된 정렬 주소에 무조건 할당되는 게 아니라서 수동으로 조정해야 함
 
   const size_t paddedReserveSize = reserveSize + alignment;
   void* ptr = mmap(NULL, paddedReserveSize, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE, -1, 0);
