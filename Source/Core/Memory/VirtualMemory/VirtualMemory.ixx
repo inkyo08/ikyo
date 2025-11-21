55807ffc5c62951a970cb7301e9462a0be738c28
Inkyo Jung <ikyoarch@gmail.com>
2025-11-21 20:00:44 +0900

    Windows VirtualMemory 구현 완료 POSIX 환경 구현 필요

---
+++ Source/Core/Memory/VirtualMemory/VirtualMemory.ixx
@@ -0,0 +1,16 @@
+﻿export module Memory:VirtualMemory;
+
+export namespace Memory::VirtualMemory {
+  // size는 시스템 페이지 크기에 맞춰 올림 처리
+  // 반환되는 범위는 항상 시스템 페이지 크기에 따라 정렬되므로, alignment를 별도로 설정해야하는 경우는 거의 없음
+  void* reserve(size_t size, size_t alignment = 0);
+
+  // base는 이전에 reserve 호출 후 반환된 값만 사용
+  // 예약할 때 사용했던 size와 alignment값을 동일하게 전달해야함
+  void unreserve(void* base, size_t size, size_t alignment = 0);
+
+  // base는 시스템 페이지 크기에 맞춰서 아래쪽으로 정렬되고, 무조건 이전에 예약한 영역 안에 있어야 함
+  // base, base + size 범위랑 겹치는 모든 페이지가 매핑 또는 언매핑됨
+  void map(void* base, size_t size);
+  void unmap(void* base, size_t size);
+}
---
+++ Source/Core/Memory/VirtualMemory/Windows.cc
@@ -0,0 +1,52 @@
+﻿module;
+
+#if _WIN32
+#include <Windows.h>
+#include <cassert>
+#endif
+
+module Memory;
+
+template<typename T>
+static inline T Align(T nData, size_t nAlign)
+{
+  assert((nAlign & (nAlign - 1)) == 0);
+  size_t size = ((size_t)nData + (nAlign - 1)) & ~(nAlign - 1);
+  return T(size);
+}
+
+const size_t systemPageSize = 4 * 1024;
+
+namespace Memory::VirtualMemory {
+#if _WIN32
+
+  void* reserve(size_t size, size_t alignment)
+  {
+    size = Align(size, systemPageSize);
+    UINT_PTR addr;
+
+    do
+    {
+      addr = reinterpret_cast<uintptr_t>(VirtualAlloc(NULL, size, MEM_RESERVE, PAGE_READWRITE));
+      if (!addr)
+        break;
+
+      UINT_PTR alignedAddr = alignment == 0 ? addr : Align(addr, alignment);
+
+      if ((alignedAddr - addr) > 0)
+      {
+        VirtualFree(reinterpret_cast<LPVOID>(addr), 0, MEM_RELEASE);
+        addr = reinterpret_cast<UINT_PTR>(VirtualAlloc(reinterpret_cast<LPVOID>(alignedAddr), size, MEM_RESERVE, PAGE_READWRITE));
+      }
+    } while (!addr);
+
+    return reinterpret_cast<void*>(addr);
+  }
+
+  void unreserve(void* base, size_t size, size_t alignment) { VirtualFree(base, 0, MEM_RELEASE); }
+
+  void map(void* base, size_t size) { VirtualAlloc(base, size, MEM_COMMIT, PAGE_READWRITE); }
+
+  void unmap(void* base, size_t size) { VirtualFree(base, size, MEM_DECOMMIT); }
+#endif
+}
