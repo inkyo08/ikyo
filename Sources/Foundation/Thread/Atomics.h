b961c4f7adacef9e9e7de708ec6c006b9e524c61
Inkyo08 <inkyo08@proton.me>
2026-01-07 12:08:44 +0900

    윈도우 환경 Atomics Thread 구현 완료 (Atomics Thread 기본 구현 전체 완료)

--- Sources/Foundation/Thread/Atomics.h
+++ Sources/Foundation/Thread/Atomics.h
@@ -26,13 +26,12 @@ namespace Ikyo::Foundation::Atomics
   inline bool compare_exchange (volatile T *ptr, T *expected, T desired)
     { return __atomic_compare_exchange_n (ptr, expected, desired, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST); }
 
-  inline bool compare_exchange (volatile __int128_t *ptr, __int128_t *expected, __int128_t desired)
-    { return __atomic_compare_exchange_n (ptr, expected, desired, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST); }
-
   template <AtomicsCompatible T>
+  requires std::is_integral_v <T>
   inline T fetch_add (volatile T *ptr, T value) { return __atomic_fetch_add (ptr, value, __ATOMIC_SEQ_CST); }
 
   template <AtomicsCompatible T>
+  requires std::is_integral_v <T>
   inline T fetch_sub (volatile T *ptr, T value) { return __atomic_fetch_sub (ptr, value, __ATOMIC_SEQ_CST); }
 
   template <AtomicsCompatible T>
@@ -49,4 +48,129 @@ namespace Ikyo::Foundation::Atomics
 } /* namespace Ikyo::Foundation::Atomics */
 
 #elif defined (_WIN32)
+#include <Windows.h>
+#include <intrin.h>
+
+namespace Ikyo::Foundation::Atomics
+{
+  template <AtomicsCompatible T>
+  inline T load (const volatile T *ptr)
+  {
+    T value = *ptr;
+    _ReadWriteBarrier ();
+    return value;
+  }
+
+  template <AtomicsCompatible T>
+  inline void store (volatile T *ptr, T value)
+  {
+    if constexpr (sizeof (T) == 1)
+      _InterlockedExchange8 ((volatile char *) ptr, (char) value);
+    else if constexpr (sizeof (T) == 2)
+      _InterlockedExchange16 ((volatile short *) ptr, (short) value);
+    else if constexpr (sizeof (T) == 4)
+      _InterlockedExchange ((volatile long *) ptr, (long) value);
+    else if constexpr (sizeof (T) == 8)
+      _InterlockedExchange64 ((volatile long long *) ptr, (long long) value);
+  }
+
+  template <AtomicsCompatible T>
+  inline T exchange (volatile T *ptr, T value)
+  {
+    if constexpr (sizeof (T) == 1)
+      return (T) _InterlockedExchange8 ((volatile char *) ptr, (char) value);
+    else if constexpr (sizeof (T) == 2)
+      return (T) _InterlockedExchange16 ((volatile short *) ptr, (short) value);
+    else if constexpr (sizeof (T) == 4)
+      return (T) _InterlockedExchange ((volatile long *) ptr, (long) value);
+    else if constexpr (sizeof (T) == 8)
+      return (T) _InterlockedExchange64 ((volatile long long *) ptr, (long long) value);
+  }
+
+  template <AtomicsCompatible T>
+  inline bool compare_exchange (volatile T *ptr, T *expected, T desired)
+  {
+    T old;
+    if constexpr (sizeof (T) == 1)
+      old = (T) _InterlockedCompareExchange8 ((volatile char *) ptr, (char) desired, (char) *expected);
+    else if constexpr (sizeof (T) == 2)
+      old = (T) _InterlockedCompareExchange16 ((volatile short *) ptr, (short) desired, (short) *expected);
+    else if constexpr (sizeof (T) == 4)
+      old = (T) _InterlockedCompareExchange ((volatile long *) ptr, (long) desired, (long) *expected);
+    else if constexpr (sizeof (T) == 8)
+      old = (T) _InterlockedCompareExchange64 ((volatile long long *) ptr, (long long) desired, (long long) *expected);
+
+    if (old == *expected)
+      return true;
+    *expected = old;
+    return false;
+  }
+
+  template <AtomicsCompatible T>
+  requires std::is_integral_v <T>
+  inline T fetch_add (volatile T *ptr, T value)
+  {
+    if constexpr (sizeof (T) == 4)
+      return (T) _InterlockedExchangeAdd ((volatile long *) ptr, (long) value);
+    else if constexpr (sizeof (T) == 8)
+      return (T) _InterlockedExchangeAdd64 ((volatile long long *) ptr, (long long) value);
+    else
+    {
+      T expected = load (ptr);
+      while (!compare_exchange (ptr, &expected, expected + value))
+        ;
+      return expected;
+    }
+  }
+
+  template <AtomicsCompatible T>
+  requires std::is_integral_v <T>
+  inline T fetch_sub (volatile T *ptr, T value)
+  {
+    return fetch_add (ptr, -value);
+  }
+
+  template <AtomicsCompatible T>
+  requires std::is_integral_v <T>
+  inline T fetch_or (volatile T *ptr, T value)
+  {
+    if constexpr (sizeof (T) == 1)
+      return (T) _InterlockedOr8 ((volatile char *) ptr, (char) value);
+    else if constexpr (sizeof (T) == 2)
+      return (T) _InterlockedOr16 ((volatile short *) ptr, (short) value);
+    else if constexpr (sizeof (T) == 4)
+      return (T) _InterlockedOr ((volatile long *) ptr, (long) value);
+    else if constexpr (sizeof (T) == 8)
+      return (T) _InterlockedOr64 ((volatile long long *) ptr, (long long) value);
+  }
+
+  template <AtomicsCompatible T>
+  requires std::is_integral_v <T>
+  inline T fetch_and (volatile T *ptr, T value)
+  {
+    if constexpr (sizeof (T) == 1)
+      return (T) _InterlockedAnd8 ((volatile char *) ptr, (char) value);
+    else if constexpr (sizeof (T) == 2)
+      return (T) _InterlockedAnd16 ((volatile short *) ptr, (short) value);
+    else if constexpr (sizeof (T) == 4)
+      return (T) _InterlockedAnd ((volatile long *) ptr, (long) value);
+    else if constexpr (sizeof (T) == 8)
+      return (T) _InterlockedAnd64 ((volatile long long *) ptr, (long long) value);
+  }
+
+  template <AtomicsCompatible T>
+  requires std::is_integral_v <T>
+  inline T fetch_xor (volatile T *ptr, T value)
+  {
+    if constexpr (sizeof (T) == 1)
+      return (T) _InterlockedXor8 ((volatile char *) ptr, (char) value);
+    else if constexpr (sizeof (T) == 2)
+      return (T) _InterlockedXor16 ((volatile short *) ptr, (short) value);
+    else if constexpr (sizeof (T) == 4)
+      return (T) _InterlockedXor ((volatile long *) ptr, (long) value);
+    else if constexpr (sizeof (T) == 8)
+      return (T) _InterlockedXor64 ((volatile long long *) ptr, (long long) value);
+  }
+} /* namespace Ikyo::Foundation::Atomics */
+
 #endif
--- Sources/Foundation/Thread/Thread.h
+++ Sources/Foundation/Thread/Thread.h
@@ -14,7 +14,7 @@ namespace Ikyo::Foundation
 {
   class Thread {
   public:
-    Thread () : handle (nullptr), joinable (false) {}
+    Thread () : handle (0), joinable (false) {}
     ~Thread () { if (joinable) abort (); }
 
     Thread (const Thread &) = delete;
@@ -37,7 +37,41 @@ namespace Ikyo::Foundation
 
 namespace Ikyo::Foundation
 {
+  inline void Thread::create (void *(*func)(void *), void *arg)
+  {
+    if (joinable) abort ();
 
+    auto *ctx = new auto ([=] () { func (arg); });
+
+    handle = reinterpret_cast <HANDLE> (_beginthreadex (
+      nullptr, 0,
+      +[] (void *p) -> unsigned
+      {
+        auto *f = (decltype (ctx)) p;
+        (*f) ();
+        delete f;
+        return 0;
+      },
+      ctx, 0, nullptr
+    ));
+
+    if (!handle)
+    {
+      delete ctx;
+      abort ();
+    }
+
+    joinable = true;
+  }
+  inline void Thread::join ()
+  {
+    if (!joinable) abort ();
+
+    WaitForSingleObject (handle, INFINITE);
+    CloseHandle (handle);
+
+    joinable = false;
+  }
 } /* namespace Ikyo::Foundation */
 
 #elif __APPLE__ || __linux__
